85eef6e24de23093a866f7d7094a56d6c718cf26
Siva123569 <nallasiva552@gmail.com>
2025-12-12 14:30:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, sivabramaiah");
      System.out.println("Hello, azevops");
-     System.out.println("Hello, siva brahmaiah"); 
+     System.out.println("Hello, sivabrahmaiah"); 
 
   }
 }
